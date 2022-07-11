db6ac329223b29c3ecc76a0efa18488203d6cd82
geltapatio <luismiguel.gamiz@gmail.com>
2022-07-11 16:32:50 +0200

    EventBus was added Basket Checkout method was implemented

--- src/Services/Basket/Basket.API/Program.cs
+++ src/Services/Basket/Basket.API/Program.cs
@@ -1,21 +1,34 @@
 using Basket.API.GrpcServices;
 using Basket.API.Repositories;
 using Discount.Grpc.Protos;
+using MassTransit;
 using Microsoft.OpenApi.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
+// Redis Configuration
 builder.Services.AddStackExchangeRedisCache(options => {
     options.Configuration = builder.Configuration.GetValue<string>("CacheSettings:ConnectionString");
 });
 
+// General Configuration
 builder.Services.AddScoped<IBasketRepository, BasketRepository>();
 builder.Services.AddAutoMapper(typeof(Program));
 
+// Grpc Configuration
 builder.Services.AddGrpcClient<DiscountProtoService.DiscountProtoServiceClient>(
     options => options.Address = new Uri(builder.Configuration["GrpcSettings:DiscountUrl"]
     ));
 builder.Services.AddScoped<DiscountGrpcServiceClient>();
+
+// MassTransit-RabbitMQ Configuration
+builder.Services.AddMassTransit(config =>
+{
+    config.UsingRabbitMq((ctx, cfg) =>
+    {
+        cfg.Host(builder.Configuration["EventBusSettings:HostAddress"]); // AMQP Advanced Message Queuing Protocol. it creates a service bus
+    });
+});
 
 builder.Services.AddSwaggerGen(options =>
 {
