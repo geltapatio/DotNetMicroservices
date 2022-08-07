5298c4efd7e1ad91e38d322e850c448cccdae019
geltapatio <luismiguel.gamiz@gmail.com>
2022-08-07 16:19:04 +0200

    Aggregator was added

---
+++ src/ApiGateways/Shopping.Aggregator/Extensions/HttpClientExtensions.cs
@@ -0,0 +1,21 @@
+﻿using System.Text.Json;
+
+namespace Shopping.Aggregator.Extensions
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <remarks>https://stackoverflow.com/questions/40027299/where-is-the-postasjsonasync-method-in-asp-net-core </remarks>
+    public static class HttpClientExtensions
+    {
+        public static async Task<T> ReadContentAs<T>(this HttpResponseMessage response)
+        {
+            if (!response.IsSuccessStatusCode)
+                throw new ApplicationException($"Something went wrong calling the API: {response.ReasonPhrase}");
+
+            var dataAsString = await response.Content.ReadAsStringAsync().ConfigureAwait(false);
+
+            return JsonSerializer.Deserialize<T>(dataAsString, new JsonSerializerOptions { PropertyNameCaseInsensitive = true });
+        }
+    }
+}
---
+++ src/ApiGateways/Shopping.Aggregator/Models/BasketModel.cs
@@ -0,0 +1,10 @@
+﻿namespace Shopping.Aggregator.Models
+{
+    public class BasketModel
+    {
+        public string UserName { get; set; }
+        //Extended mean that will retrieve some additional information from the Catalog microservices
+        public List<BasketItemExtendedModel> Items { get; set; } = new List<BasketItemExtendedModel>();
+        public decimal TotalPrice { get; set; }
+    }
+}
